068aa3aed38faffa50412a92ab9edc597af8db13
Satraan <mandiasamuels@gmail.com>
2020-05-24 22:59:16 +0200

    Updates

---
+++ precache-manifest.0e85a5b8a15fbf7ed0813edcb4eb0543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062902c9ff85fd801ee3eb34fc8881a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eae0ae109aba03c9033f",
+    "url": "/static/css/main.ff6b9735.chunk.css"
+  },
+  {
+    "revision": "dfb6e7358bc186df37c8",
+    "url": "/static/js/2.769ddad6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.769ddad6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae0ae109aba03c9033f",
+    "url": "/static/js/main.c8feee30.chunk.js"
+  },
+  {
+    "revision": "c28e18a00dcec9435f86",
+    "url": "/static/js/runtime-main.ce928be5.js"
+  },
+  {
+    "revision": "f3484a02924f1affef2af270b454b421",
+    "url": "/static/media/asteroids.f3484a02.jpg"
+  }
+]);
